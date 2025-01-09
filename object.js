175a9c8fd84f9594edab7cc2f1ac84ad98db728d
hellosiyam <hellosiyambd@gmail.com>
2025-01-09 15:57:46 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,11 @@
+const sakibKhan = {
+    name : Sakib-Khan,
+    years : 56,
+    friends :["Apu", "Raj", "Bubli"],
+    address : "Movie",
+    car : {
+        name : "tesla",
+        price : 500000,
+        made : 2025,
+    }
+}
